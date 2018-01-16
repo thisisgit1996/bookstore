9acaa2e5082fda8ca93eddf0443fc04b9a29eec2
thisisgit1996 <leo.baker@qa.com>
2018-01-16 17:08:49 +0000

    Upload.

---
+++ src/main/java/com/qa/bookstore/business/repository/BookService.java
@@ -0,0 +1,11 @@
+package com.qa.bookstore.business.repository;
+
+public interface BookService {
+	
+	String getAllBooks();
+
+	String addNewBook(String bookJson);
+
+	String deleteBook(Integer bookId);
+
+}
